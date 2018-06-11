3ec78e9c035f5b555754e27ae1fd7c84251885f8
RaphaelGuim <guimaraesraphael0203@gmail.com>
2018-06-10 22:36:54 -0300

    Paginação Na busca

--- castnautas/views.py
+++ castnautas/views.py
@@ -4,18 +4,17 @@ from django.shortcuts import render, redirect
 from castnautas.forms import *
 from castnautas.models import *
 
+
 # Create your views here.
 
-def index(request,page_num=1):
-
-
+def index(request, page_num=1):
     if request.method == 'GET':
         postagens = Postagem.objects.order_by('-data')
         pages = Paginator(postagens, 8)
 
         carrossel = Postagem.objects.order_by('-data')
 
-        carr = Paginator(carrossel,3)
+        carr = Paginator(carrossel, 3)
 
         lista_carr = carr.page(1).object_list
 
@@ -31,35 +30,36 @@ def index(request,page_num=1):
 
         return redirect('busca', tag=categoria, page_num=1)
 
-    return render(request,'index.html',{'postagens':page,'carrossel':lista_carr,'page_num':page_num, 'form':form})
+    return render(request, 'index.html',
+                  {'postagens': page, 'carrossel': lista_carr, 'page_num': page_num, 'form': form})
+
 
 def post(request, post_num):
-
     if request.method == 'GET':
-        postagem =list(Postagem.objects.filter(pk=post_num))
+        postagem = list(Postagem.objects.filter(pk=post_num))
 
         if len(postagem) == 0:
-            return redirect ('index')
+            return redirect('index')
 
         else:
             postagem = postagem[0]
-            return render(request,'post.html',{'postagem':postagem, 'form':BuscaForm()})
+            return render(request, 'post.html', {'postagem': postagem, 'form': BuscaForm()})
     else:
         categoria = request.POST['categorias']
 
         return redirect('busca', tag=categoria, page_num=1)
 
 
-def busca(request,tag,page_num=1):
-    if request.method =='GET':
+def busca(request, tag, page_num=1):
+    if request.method == 'GET':
         categorias = list(Categoria.objects.filter(nome=tag))
 
         if len(categorias) == 0:
-            return redirect ('index')
+            return redirect('index')
 
         else:
             categoria = categorias[0]
-            postagens = Postagem.objects.filter(categorias=categoria)
+            postagens = Postagem.objects.filter(categorias=categoria).order_by('-data')
             pages = Paginator(postagens, 8)
 
         carrossel = Postagem.objects.order_by('-data')
@@ -79,4 +79,13 @@ def busca(request,tag,page_num=1):
 
         return redirect('busca', tag=categoria, page_num=1)
 
-    return render(request, 'index.html', {'postagens': page, 'carrossel': lista_carr, 'page_num': page_num, 'form': BuscaForm()})
+    context = {
+        'postagens': page,
+        'carrossel': lista_carr,
+        'page_num': page_num,
+        'form': BuscaForm(),
+        'tag':tag
+
+    }
+
+    return render(request, 'index.html', context)
